d29006e4e517648548641de1d92081b6511e927a
Manuele Menozzi <manuele.menozzi@gmail.com>
2016-02-12 09:13:56 +0100

    Fix to registration.php

--- src/registration.php
+++ src/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Webgriffe_ConfigFileReader',
+    'Webgriffe_ConfigOverride',
     __DIR__
 );
